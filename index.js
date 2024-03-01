2767b83980e1e8480efe0c8f028585721ca8dd56
Dessian-Guiro <135682316+Dessian-Guiro@users.noreply.github.com>
2024-03-01 00:00:59 -0600

    added change

--- index.js
+++ index.js
@@ -1,55 +1,79 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const pg = require('pg');
-const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acme_flavors_db')
+const pg = require('pg')
+const express = require('express')
+const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/the_acme_flavors_db')
+const app = express()
 
-// app routes
 app.use(express.json());
 app.use(require('morgan')('dev'));
-app.post('/api/flavors', async (req, res, next) => {});
-app.get('/api/flavors', async (req, res, next) => {});
-app.put('/api/flavors/:id', async (req, res, next) => {});
-app.delete('/api/flavors/:id', async (req, res, next) => {});
-app.get('/api/flavors', async (req, res, next) => {
 
+//app routes
+app.post('/api/flavors', async (req, res, next) => {
     try{
-        const SQL = `SELECT * from flavors ORDER BY created_at DESC;`
-        const result = await client.query(SQL)
-         res.send(result.rows)
+        const SQL = `INSERT INTO flavors(name)
+        VALUES ($1)
+        RETURNING *`
+        const result = await client.query(SQL, [req.body.name])
+        res.send(result.rows[0])
 
-    }catch(error) {
+    }catch(error){
         next(error)
-
-    } 
+    }
 });
-    
+app.get('/api/flavors', async (req, res, next) => {
+    try{
+        const SQL = `SELECT * from flavors ORDER BY is_favorite ASC;`
+        const result = await client.query(SQL)
+        res.send(result.rows)
 
+    }catch(error){
+        next(error)
+    }
+});
+app.put('/api/flavors/:id', async (req, res, next) => {
+    try{
+        const SQL = `UPDATE flavors
+        SET name=$1, is_favorite=$2, updated_at=now()
+        WHERE id=$3 RETURNING *`
+        const result = await client.query(SQL, [req.body.name, req.body.ranking, req.params.id])
+        res.send(result.rows[0])
 
+    }catch(error){
+        next(error)
+    }
+});
+app.delete('/api/flavors/:id', async (req, res, next) => {
+    try{
+        const SQL = `DELETE FROM flavors
+        WHERE id=$1`
+        const result = await client.query(SQL, [req.params.id])
+        res.sendStatus(204)
 
-const init  = async() => {
+    }catch(error){
+        next(error)
+    }
+});
+
+const init = async () => {
     await client.connect();
-    console.log('connect to database')
-    let SQL  = `DROP TABLE IF EXISTS flavors;
-    CREATE TABLE flavors(
+    console.log('connected to database')
+    let SQL = `DROP TABLE IF EXISTS flavors;
+    CREATE TABLE notes(
     id SERIAL PRIMARY KEY,
     created_at TIMESTAMP DEFAULT now(),
     updated_at TIMESTAMP DEFAULT now(),
-    favorite INTEGER DEFAULT 3 NOT NULL,
+    is_favorite INTEGER DEFAULT 3 NOT NULL,
     name VARCHAR(255) NOT NULL
     );`
-    await client.query( SQL)
-    console.log('table created')
-    SQL = `INSERT INTO flavors(name, favorite) VALUES('orange', 5);
-    INSERT INTO flavors(name, favorite) VALUES('mango', 4);
-    INSERT INTO favors(name,  favorite) VALUES('vanilla', 2);`;
-    await client.query( SQL);
-    console.log('data seeded')
+    await client.query(SQL)
+    console.log('tables created')
+    SQL = ` INSERT INTO notes(name, is_favorite) VALUES('mango', 5);
+    INSERT INTO flavors(name, is_fovorite) VALUES('vanilla', 4);
+    INSERT INTO flavors(name, is_favorite) VALUES('stawberry', 2);`;
+    await client.query(SQL);
+    console.log('data seeded');
 
     const port = process.env.PORT || 3000
     app.listen(port, () => console.log(`listening on port ${port}`))
-    
-
-    
 }
-init();
+
+init()
